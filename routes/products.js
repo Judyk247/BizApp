7a8f20bdd2f05bc26288d60d4818d32e9e759c40
Judyk247 <judechukwunalua@gmail.com>
2026-03-22 17:02:43 +0100

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const { getProducts, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
-const { authenticate, authorize } = require('../middleware/auth');
-const router = express.Router();
 
-router.get('/', authenticate, getProducts);
-router.post('/', authenticate, authorize('admin', 'manager'), createProduct);
-router.put('/:id', authenticate, authorize('admin', 'manager'), updateProduct);
-router.delete('/:id', authenticate, authorize('admin'), deleteProduct);
-
-module.exports = router;
