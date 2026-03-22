552c0e566e5a5afc61b62af83d6e076459f3331e
Judyk247 <judechukwunalua@gmail.com>
2026-03-22 17:03:16 +0100

    Update purchases.js

--- routes/purchases.js
+++ routes/purchases.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const { createPurchase, getPurchaseById } = require('../controllers/purchaseController');
-const { authenticate, authorize } = require('../middleware/auth');
-const router = express.Router();
 
-router.post('/', authenticate, authorize('admin', 'manager'), createPurchase);
-router.get('/:id', authenticate, getPurchaseById);
-
-module.exports = router;
