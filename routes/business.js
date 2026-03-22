b26671ca50ac95b268e37a7fd9ed88942f525ee0
Judyk247 <judechukwunalua@gmail.com>
2026-03-22 16:59:45 +0100

    Update business.js

--- routes/business.js
+++ routes/business.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const { getBusiness, updateBusiness, regenerateInviteCode } = require('../controllers/businessController');
-const { authenticate, authorize } = require('../middleware/auth');
-const router = express.Router();
 
-router.get('/', authenticate, getBusiness);
-router.put('/', authenticate, updateBusiness);
-router.put('/invite-code', authenticate, authorize('admin'), regenerateInviteCode);
-
-module.exports = router;
