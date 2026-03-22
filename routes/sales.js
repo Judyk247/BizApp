48fe89846965060bfb947bf5afd6bfffe0b65b61
Judyk247 <judechukwunalua@gmail.com>
2026-03-22 17:04:53 +0100

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const { createSale, getSaleById, getSales } = require('../controllers/saleController');
-const { authenticate } = require('../middleware/auth');
-const router = express.Router();
 
-router.post('/', authenticate, createSale);
-router.get('/', authenticate, getSales);
-router.get('/:id', authenticate, getSaleById);
-
-module.exports = router;
