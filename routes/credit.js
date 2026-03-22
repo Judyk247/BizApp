8cf1c3b10f26c4f57c69311c0405d546f18bc467
Judyk247 <judechukwunalua@gmail.com>
2026-03-22 17:00:41 +0100

    Update credit.js

--- routes/credit.js
+++ routes/credit.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const { getCustomerCredit, recordPayment } = require('../controllers/creditController');
-const { authenticate } = require('../middleware/auth');
-const router = express.Router();
 
-router.get('/customer/:customerId', authenticate, getCustomerCredit);
-router.post('/payment', authenticate, recordPayment);
-
-module.exports = router;
