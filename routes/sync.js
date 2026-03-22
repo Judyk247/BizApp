d859e7e5568d6758f4a54ab76e34953317c97763
Judyk247 <judechukwunalua@gmail.com>
2026-03-22 17:05:22 +0100

    Update sync.js

--- routes/sync.js
+++ routes/sync.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const { uploadChanges } = require('../controllers/syncController');
-const { authenticate } = require('../middleware/auth');
-const router = express.Router();
 
-router.post('/', authenticate, uploadChanges);
-
-module.exports = router;
